a55eaef3420ecf7e2f56fc52d0b63b97adb421eb
Stéphanie <stef-x06@hotmail.fr>
2020-04-26 16:43:52 +0200

    fix db data

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -1,8 +1,16 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: DataTypes.STRING,
-    pasword: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      is: /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/i
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   }, {});
   return User;
 };
